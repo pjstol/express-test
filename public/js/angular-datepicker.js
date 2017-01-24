58e7d32c875c16268821d8039b850688b813964e
Pedro Diaz <pds@injenia.cl>
2017-01-24 06:41:04 -0300

    Agrega Fotos

--- public/js/angular-datepicker.js
+++ public/js/angular-datepicker.js
@@ -1,19 +1,30 @@
 angular.module('datepickerBasicUsage',
     ['ngMaterial', 'ngMessages']).controller('AppCtrl', function($scope) {
+
   $scope.myDate = new Date();
 
-  $scope.minDate = new Date(
-      $scope.myDate.getFullYear(),
-      $scope.myDate.getMonth() - 2,
-      $scope.myDate.getDate());
+  $scope.myDate.year = $scope.myDate.getFullYear();
+  $scope.myDate.month = $scope.myDate.getMonth()+1;
+  $scope.myDate.date = $scope.myDate.getDate();
 
-  $scope.maxDate = new Date(
-      $scope.myDate.getFullYear(),
-      $scope.myDate.getMonth() + 2,
-      $scope.myDate.getDate());
+  var stringBusqueda = $scope.year + $scope.month + $scope.date;
 
-  $scope.onlyWeekendsPredicate = function(date) {
-    var day = date.getDay();
-    return day === 0 || day === 6;
-  };
+  // $scope.myDate = new Date();
+
+  // $scope.minDate = new Date(
+  //     $scope.myDate.getFullYear(),
+  //     $scope.myDate.getMonth() - 2,
+  //     $scope.myDate.getDate());
+  //
+  // $scope.maxDate = new Date(
+  //     $scope.myDate.getFullYear(),
+  //     $scope.myDate.getMonth() + 2,
+  //     $scope.myDate.getDate());
+
+  // $scope.onlyWeekendsPredicate = function(date) {
+  //   var day = date.getDay();
+  //   return day === 0 || day === 6;
+  // };
+
+
 });

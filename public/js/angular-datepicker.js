0982ee742639100015ff432ee8d6e060e2f76e11
Pedro Diaz <pds@injenia.cl>
2017-01-25 05:49:47 -0300

    some change

--- public/js/angular-datepicker.js
+++ public/js/angular-datepicker.js
@@ -1,19 +1,19 @@
 /*global angular*/
 angular.module('datepickerBasicUsage',
     ['ngMaterial', 'ngMessages']).controller('AppCtrl', function($scope) {
-        
+
   $scope.myDate = new Date();
-  
+
   $scope.myDate.year = ""+$scope.myDate.getFullYear();
   $scope.myDate.month = ("0" + ($scope.myDate.getMonth()+1)).slice(-2);
   $scope.myDate.date = ("0" + $scope.myDate.getDate()).slice(-2);
   $scope.myDate.stringBusqueda = ""+$scope.myDate.year+$scope.myDate.month+$scope.myDate.date;
-  
+
   $scope.reqDate = function(){
     $scope.myDate.year = ""+$scope.myDate.getFullYear();
     $scope.myDate.month = ("0" + ($scope.myDate.getMonth()+1)).slice(-2);
     $scope.myDate.date = ("0" + $scope.myDate.getDate()).slice(-2);
     $scope.myDate.stringBusqueda = ""+$scope.myDate.year+$scope.myDate.month+$scope.myDate.date;
     }
-  $scope.times = ["10:20", "10:50", "12:10", "15:00", "18:00"];
+  $scope.times = ["10:20", "11:35", "12:50", "13:20", "14:55"];
 });

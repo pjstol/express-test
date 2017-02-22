cca5fbd5d611f2d6d05fe243cf06b26f104498d5
Pedro Diaz <pds@injenia.cl>
2017-02-21 21:22:46 -0300

    Images now potentially working

--- public/js/angular-datepicker.js
+++ public/js/angular-datepicker.js
@@ -6,10 +6,11 @@ angular.module('myApp', ['ngMaterial']).controller('AppCtrl', function($scope,$h
   $scope.myDate.year = ""+$scope.myDate.getFullYear();
   $scope.myDate.month = ("0" + ($scope.myDate.getMonth()+1)).slice(-2);
   $scope.myDate.date = ("0" + $scope.myDate.getDate()).slice(-2);
-  $scope.myDate.stringBusqueda = ""+$scope.myDate.year+$scope.myDate.month+$scope.myDate.date;
+  $scope.myDate.stringBusqueda = ""+$scope.myDate.year+
+                                    $scope.myDate.month+
+                                    $scope.myDate.date;
 
   $scope.times = ["10:20", "11:35", "12:50", "13:20", "14:55"];
-  $scope.alfa = "http://placekitten.com.s3.amazonaws.com/homepage-samples/408/287.jpg";
   $scope.fotos = [];
   $scope.triton = [];
 
@@ -29,18 +30,20 @@ angular.module('myApp', ['ngMaterial']).controller('AppCtrl', function($scope,$h
         }
         $scope.proyecto = $scope.triton[20].id.substr(0,5);
         $scope.camara =  $scope.triton[20].id.substr(5,4);
-        $scope.alfa = "https://storage.googleapis.com/" + response.data.message[response.data.message.length-1].bucket.id + "/" + response.data.message[response.data.message.length-1].id;
-
+        $scope.alfa = "https://storage.googleapis.com/" +
+                      $scope.triton[$scope.triton.length-1].bucket.id + "/" +
+                      $scope.triton[$scope.triton.length-1].id;
       });
 
   $scope.selectChanged = function(){
-    $scope.alfa = "https://storage.googleapis.com/" + $scope.selectedFoto.bucket.id + "/" + $scope.selectedFoto.id;
+    $scope.alfa = "https://storage.googleapis.com/" +
+                  $scope.selectedFoto.bucket.id + "/" +
+                  $scope.selectedFoto.id;
   }
 
   $scope.reqDate = function(){
-    $scope.myDate.year = ""+$scope.myDate.getFullYear();
-    $scope.myDate.month = ("0" + ($scope.myDate.getMonth()+1)).slice(-2);
-    $scope.myDate.date = ("0" + $scope.myDate.getDate()).slice(-2);
-    $scope.myDate.stringBusqueda = ""+$scope.myDate.year+$scope.myDate.month+$scope.myDate.date;
+    //función de busqueda para dejar solo horas en el select de la hora
+    //y seleccionar las fotos.
+    $scope.times = $scope.triton.name;
     }
 });
